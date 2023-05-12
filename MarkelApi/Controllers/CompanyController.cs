d071135ad7661f5c64d8309682e18e5be12399b0
shamsherchoudhury <42291196+shamsherchoudhury@users.noreply.github.com>
2023-05-12 17:23:14 +0100

    Checkpoint

--- MarkelApi/Controllers/CompanyController.cs
+++ MarkelApi/Controllers/CompanyController.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace markelapi.Controllers
 {
     [Route("api/[controller]")]
-    [AllowAnonymous]
     public class CompanyController : Controller
     {
         // GET: api/values
