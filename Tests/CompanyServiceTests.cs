12ab180d0cfc47b924608d757dd14fba725933cb
shamsherchoudhury <42291196+shamsherchoudhury@users.noreply.github.com>
2023-05-17 08:39:19 +0100

    Add one unit test (MVP)

---
+++ Tests/CompanyServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using MarkelApi.Services.Interfaces;
+using MarkelApi.Repositories.Interfaces;
+using NSubstitute;
+using MarkelApi.Services;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class CompanyServiceTests
+    {
+        private ICompanyRepository _companyRepository;
+        private IClaimRepository _claimRepository;
+        private ICompanyService _companyService;
+
+        [SetUp]
+        public void Setup()
+        {
+            _companyRepository = Substitute.For<ICompanyRepository>();
+            _claimRepository = Substitute.For<IClaimRepository>();
+
+            _companyService = new CompanyService(_companyRepository, _claimRepository);
+        }
+
+        [Test]
+        public async Task GetCompany_Match_Name()
+        {
+            const string companyName = "Partners Inc.";
+            _companyRepository.GetCompany(Arg.Any<int>()).Returns(new Models.Entities.Company { Name = companyName });
+
+            var company = await _companyService.GetCompany(323);
+
+            Assert.NotNull(company);
+            Assert.That(company.Name,Is.EqualTo(companyName));
+        }
+    }
+}
